536d80728769d380840263729d6fc5a488011c8f
Muhammet <muhammeta7@gmail.com>
2016-11-22 12:22:07 -0500

    initial site files

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+var PORT = 3000;
+
+app.get('/', function (req, res) {
+  res.redirect('index');
+});
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+  console.log('App listening on PORT ' + PORT);
+});
